d1f34684b00ab3f4117efa8431d57a156d35753f
Zoudrick <r08rodri@gmail.com>
2024-06-02 17:39:03 -0600

    2024-06-02_RH
    Actualización de enemigos y sistema de guardado

--- Assets/GamePad.cs
+++ Assets/GamePad.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GamePad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject opcionesFirst; 
+    public GameObject principalFirst;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
